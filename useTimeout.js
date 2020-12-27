2fefe50127204a1bec2d7a75c3c87e55249a6bd5
julination <julianoh1909@gmail.com>
2020-12-27 09:43:40 -0300

    useTimeout

---
+++ useTimeout.js
@@ -0,0 +1,31 @@
+//snippet
+const useTimeout = (callback, delay) => {
+  const savedCallback = React.useRef();
+
+  React.useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  React.useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+
+    if (delay !== null) {
+      let id = setTimeout(tick, delay);
+      return () => clearTimeout(id);
+    }
+  }, [delay]);
+};
+
+//usage
+
+const Component = (prosp) => {
+  const [seconds, setSeconds] = React.useState(0);
+
+  useTimeout(() => {
+    setSeconds(seconds + 1);
+  }, 5000);
+
+  return <p>{seconds}</p>;
+};
